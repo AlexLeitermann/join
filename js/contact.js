dc0c85976a45c6b8122eb59c5be866cb68c116d1
Alexander Leitermann <alexander@leitermann-home.de>
2024-02-04 16:25:46 +0100

    insert edit contact now

--- js/contact.js
+++ js/contact.js
@@ -83,6 +83,9 @@ function newContactView(event) {
     const elem = event.target;
     const elemList = event.currentTarget;
     if(elem === elemList || elemList.id === "addcontact-head-close" || elemList.id === "addcontact-btn" || elemList.id === "addcontact-cancel-btn" ) {
+        document.getElementById('addcontact-name').value = '';
+        document.getElementById('addcontact-email').value = '';
+        document.getElementById('addcontact-phone').value = '';
         document.getElementById('addcontact-main').classList.toggle(active);
     }
 }
@@ -198,6 +201,7 @@ function renderListEntry(i, isUser = "") {
 // ############################################################
 // ############################################################
 // ############################################################
+// Contact Detail View
 /**
  * Renders the detailed view of a contact.
  * 
@@ -211,8 +215,123 @@ function renderSingleView(id) {
     document.getElementById("contact-single-info-email-text").innerHTML = contactsSorted[index].email;
     document.getElementById("contact-single-info-phone-text").innerHTML = contactsSorted[index].phone;
     document.getElementById("contact-single-info-badge-circle").className = `badge-120 bdg-${contactsSorted[index]["badgecolor"]}`;
-    // document.getElementById("options").innerHTML = renderOptions(id);
-    // document.getElementById("contact-single-info-options").innerHTML = renderOptions(id);
+
+    document.getElementById("contactview-options").innerHTML = renderSingleViewOptions(id);
+}
+
+
+function renderSingleViewOptions(id) {
+    return `
+        <button id="contact-edit" class="contact-options" onclick="editContactPopup(${id})">
+            <img src="./img/icon_edit_main.png"><span>Edit</span>
+        </button>
+        <button id="contact-delete" class="contact-options" onclick="deleteContact(${id})">
+            <img src="./img/icon_delete_main.png"><span>Delete</span>
+        </button>
+    `;
+}
+
+
+// ############################################################
+// ############################################################
+// ############################################################
+// ADD Contact
+
+
+
+
+// ############################################################
+// ############################################################
+// ############################################################
+// Edit Contact
+function editContactPopup(id) {
+    let index = idToIndex(id, contactsSorted);
+    document.getElementById("editBadge120-text").innerHTML = contactsSorted[index].initials;
+    document.getElementById("editBadge120").className = `badge-120 bdg-${contactsSorted[index]["badgecolor"]}`;
+    document.getElementById("editcontact-id").value = contactsSorted[index].id;
+    document.getElementById("editcontact-name").value = contactsSorted[index].name;
+    document.getElementById("editcontact-email").value = contactsSorted[index].email;
+    document.getElementById("editcontact-phone").value = contactsSorted[index].phone;
+    document.getElementById("editcontact-content-actionbox").innerHTML = `
+        <button type="button" class="btn-secondary" id="editcontact-delete-btn" onclick='deleteContact(${id})'><span>Delete</span></button>
+        <button type="submit" class="btn-primary"><span>Save</span><img src="./img/icon_chk_btn_wh.png"></button>
+    `;
+    document.getElementById('editcontact-main').classList.toggle(active);
+}
+
+
+function deleteContact(id) {
+    console.log('Delete Contact now', id);
+}
+
+
+async function saveEditContact() {
+    let id = +document.getElementById("editcontact-id").value;
+    let index = idToIndex(id, contactList);
+    await updateContactFields(index);
+    // await saveData("contacts-id"+cuid, contacts);
+    updateLocalStorage(index);
+    contactsSorted = sortContacts(contacts);
+    renderSaveEditContact(id);
+}
+
+
+/**
+ * Changes the contact details with the input from the form fields.
+ * 
+ * @param {number} index - Index for the contact details in the contact list
+ */
+async function updateContactFields(index) {
+    contactList[index].name = document.getElementById("editconname").value;
+    contactList[index].initials = initialsFrom(document.getElementById("editconname").value);
+    contactList[index].email = document.getElementById("editconemail").value;
+    contactList[index].phone = document.getElementById("editconphone").value;
+    await updateUserFields(index);
+}
+
+
+/**
+ * If contact is also a user, update the user details too.
+ * 
+ * @param {number} index - Index for the contact details in the contact list
+ */
+async function updateUserFields(index) {
+    let userIndex = idToIndex(contactList[index].userid, userList);
+    if(isCurrentUser(contactList[index].userid)) {
+        userList[userIndex].name = contactList[index].name;
+        userList[userIndex].initials = contactList[index].initials;
+        userList[userIndex].email = contactList[index].email;
+        userList[userIndex].phone = contactList[index].phone;
+        await saveData("users", userList);
+    }
+}
+
+
+/**
+ * Updated contact details can also be changed in LocalStorage for the logged in user.
+ * 
+ * @param {*} index - Index for the contact details in the contact list
+ */
+function updateLocalStorage(index) {
+    localStorage.setItem('loggedInUser', contactList[index].name);
+    if(localStorage.getItem("rememberEmail")) {
+        localStorage.setItem('rememberEmail', contactList[index].email);
+    }
+    localStorage.setItem('loggedInUser', contactList[index].name);
+    loggedInUser = localStorage.getItem("loggedInUser");
+}
+
+
+/**
+ * After changing contact information, re-render all areas for it.
+ * 
+ * @param {number} id - Id of the selected contact.
+ */
+function renderSaveEditContact(id) {
+    renderHeaderUserName();
+    renderContactList();
+    renderSingleView(id);
+    openEditCon();
 }
 
 
